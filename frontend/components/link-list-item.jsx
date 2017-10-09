bba8846f6117f99f98e40097631e8ddfe18cdacf
Nate Festinger <nate.festinger@gmail.com>
2017-10-09 11:11:51 -0700

    Add contrast to link text and background

--- frontend/components/link-list-item.jsx
+++ frontend/components/link-list-item.jsx
@@ -6,14 +6,21 @@ import { Link } from 'react-router-dom';
 const LINK_LIST_ITEM_WIDTH = 200; // width in px
 const LINK_LIST_ITEM_HEIGHT = 30; // height in px
 
-const LinkListItem = ({ link }) => {
-
-  const styles = {
+const generateStyles = () => {
+  let styles = {
     top: _.random(0, window.innerHeight - LINK_LIST_ITEM_HEIGHT),
     left: _.random(0, window.innerWidth - LINK_LIST_ITEM_WIDTH),
-    color: Color.random(),
     background: Color.random()
   };
+
+  let fontColor = styles.background.getContrastYIQ();
+
+  return { ...styles, color: fontColor };
+}
+
+const LinkListItem = ({ link }) => {
+
+  const styles = generateStyles();
 
   return (
     <Link to={ link.id }>
--- frontend/util/color.js
+++ frontend/util/color.js
@@ -1,31 +1,28 @@
 import _ from 'lodash';
 
+const COLOR_MAX = 255;
+const MIN_CONTRAST = 3.5;
+
 class Color {
-
+  // color is { red, green, blue }
   static random() {
-    const red   = _.random(0, 255);
-    const green = _.random(0, 255);
-    const blue  = _.random(0, 255);
-
-    return new Color(red, green, blue);
+    return new Color(
+      Color.randomRGBValue(),
+      Color.randomRGBValue(),
+      Color.randomRGBValue()
+    );
   }
 
   static randomRGBValue() {
-    return _.random(0, 255);
+    return _.random(COLOR_MAX);
   }
 
   static fromString(colorString) {
     const rgb = colorString.slice(4).slice(0, -1)
-    .split(', ').map(num => parseInt(num));
+    .split(', ')
+    .map((num) => parseInt(num));
 
     return new Color(...rgb);
-  }
-
-  static randomHue() {
-    const hues = ['red', 'green', 'blue'];
-    const randIdx = _.random(0, hues.length - 1);
-
-    return hues[randIdx];
   }
 
   constructor(red, green, blue) {
@@ -34,8 +31,59 @@ class Color {
     this.blue = blue;
   }
 
+  contrastsWith(otherColor) {
+    return Color.contrast(this, otherColor) >= MIN_CONTRAST;
+  }
+
+  mask(color2) {
+    // return average of self and color2
+    let red = (this.red + color2.red) / 2;
+    let green = (this.green + color2.green) / 2;
+    let blue = (this.blue + color2.blue) / 2;
+
+    return new Color(red, green, blue);
+  }
+
+  toCssString() {
+    const [red, green, blue] =
+    [Math.floor(this.red), Math.floor(this.green), Math.floor(this.blue)];
+
+    return `rgb(${red}, ${green}, ${blue})`
+  }
+
   toString() {
-    return `rgb(${this.red}, ${this.green}, ${this.blue})`
+    return `rgb(${this.red}, ${this.green}, ${this.blue})`;
+  }
+
+  get luminance() {
+    let hues = [this.red, this.green, this.blue];
+
+    hues = hues.map(hue => {
+      hue /= COLOR_MAX;
+      return hue <= 0.03928 ? hue / 12.92 : Math.pow( (hue + 0.055) / 1.055, 2.4 );
+    });
+
+    return hues[0] * 0.2126 + hues[1] * 0.7152 + hues[2] * 0.0722;
+  }
+
+  static contrast(color1, color2) {
+    return (color1.luminance + 0.05) / (color2.luminance + 0.05);
+  }
+
+  get lightness() {
+    const red = this.red / 255;
+    const green = this.green / 255;
+    const blue = this.blue / 255;
+
+    const cmin = Math.min(red, green, blue);
+    const cmax = Math.max(red, green, blue);
+
+    return (cmax + cmin) / 2;
+  }
+
+  getContrastYIQ() {
+  	let yiq = ((this.red * 299) + (this.green * 587) + (this.blue * 114)) / 1000;
+  	return (yiq >= 128) ? new Color(0, 0, 0) : new Color(255, 255, 255);
   }
 }
 
