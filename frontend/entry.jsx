e84f2d31b37232d375869937b0b8b0bad74983bb
John Hartman <john.f.d.hartman@gmail.com>
2017-10-12 13:17:16 -0700

    added frontend api request for pagedata

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './root';
+import {configureStore} from './store/store';
 const fs = require('fs');
 
 document.addEventListener('DOMContentLoaded', () => {
+  let store = configureStore({});
+  window.store = store;
   const root = document.getElementById('root');
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
