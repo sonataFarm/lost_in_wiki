1c6c1e98a916cd9591fe9ca700dce81386e89cac
John Hartman <john.f.d.hartman@gmail.com>
2017-10-12 13:57:17 -0700

    api uses title as wildcard

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get '*path', to: 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :pages, only: [:show]
+    get '/pages/:title', to: 'pages#show'
   end
 
 end
