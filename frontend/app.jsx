b4939ed3a74741506c46d94f376ec0e48cce9e35
Nate Festinger <nate.festinger@gmail.com>
2017-10-13 13:00:28 -0700

    Get a workable entry file to debug

--- frontend/app.jsx
+++ frontend/app.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import LinkFlowContainer from './components/link_flow/link-flow-container';
-import StarFieldContainer from './components/star-field-container ';
+import StarField from './components/star-field';
 
 // !!! testing
 import _ from 'lodash';
@@ -10,7 +10,7 @@ import hitlerLinks from './util/hitler-links';
 
 const App = props => (
   <Switch>
-    <Route exact path='/' component={StarFieldContainer}/>
+    <Route exact path='/' render={() => <StarField links={ links } />}/>
     <Route path='/linkFlow' component={ LinkFlowContainer } />
   </Switch>
 );
