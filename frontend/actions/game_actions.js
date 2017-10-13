4279b36dea6f2cf473ab2e12b962d8b6bdda5cd1
John Hartman <john.f.d.hartman@gmail.com>
2017-10-12 17:15:10 -0700

    added action and reducer for new game

--- frontend/actions/game_actions.js
+++ frontend/actions/game_actions.js
@@ -1,6 +1,7 @@
 export const UPDATE_CURRENT_PAGE = 'UPDATE_CURRENT_PAGE';
 export const UPDATE_FOCUS_PAGE = 'UPDATE_FOCUS_PAGE';
 export const ADD_PAGE_TO_HISTORY = 'ADD_PAGE_TO_HISTORY';
+export const START_NEW_GAME = 'START_NEW_GAME';
 
 export const updateCurrentPage = (pageTitle) => ({
   type: UPDATE_CURRENT_PAGE,
@@ -15,4 +16,10 @@ export const updateFocusPage = (pageTitle) => ({
 export const addPageToHistory = (pageTitle) => ({
   type: ADD_PAGE_TO_HISTORY,
   pageTitle
+});
+
+export const startNewGame = (originTitle, destinationTitle) => ({
+  type: START_NEW_GAME,
+  originTitle,
+  destinationTitle
 });
--- frontend/reducers/game_reducer.js
+++ frontend/reducers/game_reducer.js
@@ -2,13 +2,22 @@ import merge from 'lodash/merge';
 import {
   UPDATE_CURRENT_PAGE,
   UPDATE_FOCUS_PAGE,
-  ADD_PAGE_TO_HISTORY
+  ADD_PAGE_TO_HISTORY,
+  START_NEW_GAME
 } from '../actions/game_actions.js';
 
 export const gameReducer = (gameSlice = {}, action) => {
   Object.freeze(gameSlice);
   let newSlice;
   switch(action.type) {
+    case START_NEW_GAME:
+      newSlice = {
+        origin: action.originTitle,
+        destination: action.destinationTitle,
+        currentPage: action.originTitle,
+        focusPage: action.originTitle
+      };
+
     case UPDATE_CURRENT_PAGE:
       newSlice = merge({}, gameSlice);
       newSlice.currentPage = action.pageTitle;
@@ -23,7 +32,7 @@ export const gameReducer = (gameSlice = {}, action) => {
       newSlice = merge({}, gameSlice);
       newSlice.pageHistory.push(action.pageTitle);
       return newSlice;
-      
+
     default:
       return gameSlice;
   }
