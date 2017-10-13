428ddacdb7171307dbbb237b9e71bc7b5e146f8a
Nate Festinger <nate.festinger@gmail.com>
2017-10-13 12:48:48 -0700

    More webpack diffing

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,12 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: [/\.jsx?$/],
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
         query: {
           presets: ['es2015', 'react', 'stage-2']
         }
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
       }
     ]
   },
