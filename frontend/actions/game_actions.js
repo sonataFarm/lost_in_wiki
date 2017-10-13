659dcba5c2c45a2a0c0843b7c3635ee3d0d23dca
John Hartman <john.f.d.hartman@gmail.com>
2017-10-12 17:00:57 -0700

    added action and reducer for focus page

--- frontend/actions/game_actions.js
+++ frontend/actions/game_actions.js
@@ -1,6 +1,11 @@
 export const UPDATE_CURRENT_PAGE = 'UPDATE_CURRENT_PAGE';
-
+export const UPDATE_FOCUS_PAGE = 'UPDATE_FOCUS_PAGE';
 export const updateCurrentPage = (pageTitle) => ({
   type: UPDATE_CURRENT_PAGE,
+  pageTitle
+});
+
+export const updateFocusPage = (pageTitle) => ({
+  type: UPDATE_FOCUS_PAGE,
   pageTitle
 });
--- frontend/reducers/game_reducer.js
+++ frontend/reducers/game_reducer.js
@@ -1,5 +1,8 @@
 import merge from 'lodash/merge';
-import {UPDATE_CURRENT_PAGE} from '../actions/game_actions.js';
+import {
+  UPDATE_CURRENT_PAGE,
+  UPDATE_FOCUS_PAGE
+} from '../actions/game_actions.js';
 
 export const gameReducer = (gameSlice = {}, action) => {
   Object.freeze(gameSlice);
@@ -8,6 +11,11 @@ export const gameReducer = (gameSlice = {}, action) => {
     case UPDATE_CURRENT_PAGE:
       newSlice = merge({}, gameSlice);
       newSlice.currentPage = action.pageTitle;
+      return newSlice;
+
+    case UPDATE_FOCUS_PAGE:
+      newSlice = merge({}, gameSlice);
+      newSlice.focusPage = action.pageTitle;
       return newSlice;
 
     default:
