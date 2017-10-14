4b741c6783384a3e671a8c3623b1a497e7225c4c
John Hartman <john.f.d.hartman@gmail.com>
2017-10-13 19:59:47 -0700

    added route for api request

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#root'
   namespace :api, defaults: {format: :json} do
     get '/pages/:title', to: 'pages#show'
+    get '/pages/page_ranks', to: 'pages#page_ranks'
   end
   get '*path', to: 'static_pages#root'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
